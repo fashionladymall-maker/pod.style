142811ba7a162020aa3c4e7afe9f970366f75624
PianoMusic R <fashionladymall@gmail.com>
2025-09-21 16:06:17 +0000

    在登录页面还是将匿名登录释放出来

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,7 +1,7 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
+import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 
 const firebaseConfig = {
   "projectId": "studio-1269295870-5fde0",
@@ -17,17 +17,17 @@ const firebaseConfig = {
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 
 // Initialize App Check
-// if (typeof window !== 'undefined') {
-//   // Set to true to get a debug token in the console.
-//   // This can be used to test App Check without a real reCAPTCHA provider.
-//   // Make sure to remove this in production.
-//   (window as any).FIREBASE_APPCHECK_DEBUG_TOKEN = true;
+if (typeof window !== 'undefined') {
+  // Set to true to get a debug token in the console.
+  // This can be used to test App Check without a real reCAPTCHA provider.
+  // Make sure to remove this in production.
+  (window as any).FIREBASE_APPCHECK_DEBUG_TOKEN = true;
 
-//   initializeAppCheck(app, {
-//     provider: new ReCaptchaV3Provider(process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY!),
-//     isTokenAutoRefreshEnabled: true
-//   });
-// }
+  initializeAppCheck(app, {
+    provider: new ReCaptchaV3Provider(process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY!),
+    isTokenAutoRefreshEnabled: true
+  });
+}
 
 
 const auth = getAuth(app);
