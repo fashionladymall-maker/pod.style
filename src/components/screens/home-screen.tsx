a694baac1436dd25b13486f60914810e4dde59d3
PianoMusic R <fashionladymall@gmail.com>
2025-09-21 17:17:50 +0000

    太占篇幅了，小巧精致 (_for element div_)

--- src/components/screens/home-screen.tsx
+++ src/components/screens/home-screen.tsx
@@ -142,11 +142,11 @@ const HomeScreen: React.FC<HomeScreenProps> = ({
   return (
     <div className="flex flex-col h-full">
       <ScrollArea className="flex-grow">
-        <div className="p-6 space-y-8">
+        <div className="p-6 space-y-6">
             
-            <div className="grid grid-cols-2 gap-4">
-                <Button variant="outline" className="h-16 text-base"><Sparkles className="mr-2"/>流行创意</Button>
-                <Button variant="outline" className="h-16 text-base"><TrendingUp className="mr-2"/>定制排行</Button>
+            <div className="grid grid-cols-2 gap-3">
+                <Button variant="outline" className="h-12 text-sm"><Sparkles className="mr-2"/>流行创意</Button>
+                <Button variant="outline" className="h-12 text-sm"><TrendingUp className="mr-2"/>定制排行</Button>
             </div>
 
             {publicCreations.length > 0 && <Separator />}
